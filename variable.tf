5ce432be0a2a83bdd7c3b3b939cda54932c9d3dd
aeobomese <125628276+aeobomese@users.noreply.github.com>
2023-02-18 20:37:33 -0600

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,41 @@
+variable "access_key" {
+  description = "provides access key"
+  type        = string
+  default     = "AKIAVSZ6KNAC4FQARZXM"
+
+}
+
+variable "secret_key" {
+  description = "secret access key"
+  type        = string
+  default     = "CKlHmwfYr/L0W57v7qzoVvSoh0JySHC8SCCuYheK"
+
+}
+
+variable "storage" {
+  description = "creating storage"
+  type        = string
+  default     = "20"
+
+}
+
+variable "engine_version" {
+  description = "creating engine version"
+  type        = string
+  default     = "5.7"
+
+}
+
+variable "username" {
+  description = "creating username"
+  type        = string
+  default     = "alex"
+
+}
+
+variable "password" {
+  description = "creating password"
+  type        = string
+  default     = "Regine#00"
+
+}
